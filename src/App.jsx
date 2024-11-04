2c439aff050e9776a0e77efa332a00de49a4c691
FeedW3 <lamourenzo@gmail.com>
2024-11-04 18:24:41 +0100

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './HomePage';
 import ContactPage from './ContactPage';
 
 function App() {
+    const basename = import.meta.env.MODE === "production" ? "/portfolio" : "";
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/contacts" element={<ContactPage />} />
-            </Routes>
-        </Router>
+        <BrowserRouter basename={basename}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/contacts" element={<ContactPage />} />
+                </Routes>
+            </Router>
+        </BrowserRouter>
     );
 }
 
